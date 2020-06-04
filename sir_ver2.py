e91e7b1986bd75c096db0f980ee7d3ae4f62f8e9
Jorge Marcelo Risco <61189874+jorgemrisco@users.noreply.github.com>
2020-06-03 23:32:51 -0300

    travel added

--- sir_ver2.py
+++ sir_ver2.py
@@ -1,12 +1,13 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import math
 #SIR with vital dynamics
 
 # Total population, N.
 N = 1000
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 1, 0
+I0, R0, M0 = 1, 0, 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
@@ -14,24 +15,30 @@ beta, gamma = 0.2, 1./10
 # A grid of time points (in days)
 t = np.linspace(0, 250, 250)
 #Natality rate
-m = 0.0118
+m = 0.00338
 #Death Rate (D1 é morte dos saudáveis, D2 é morte dos infectados)
-D1,D2 = 0.0086,0.0086
+D1,D2 = 0.00243,0.026
+es = 3.0 #entrada de S
+ei = 2.0 #Entrada de i
+ss = 2.0 #Saida de s
+si = 0.5 #Saida de i
+
 
 # The SIR model differential equations.
 def deriv(y, t, beta, gamma):
-    S, I, R = y
+    S, I, R, M = y
     N = S + I + R
-    dSdt = (m*N) -beta * S * I / N - (D1*S)
-    dIdt = beta * S * I / N - gamma * I - (D2*I)
+    dSdt = es*math.exp(-0.04*t) - ss*math.exp(-0.04*t) + (m*N) -beta * S * I / N - (D1*S)
+    dIdt = ei*math.exp(-0.04*t) - si*math.exp(-0.04*t) +beta * S * I / N - gamma * I - (D2*I)
     dRdt = gamma * I - (D1*R)
-    return dSdt, dIdt, dRdt
+    dMdt = D2*I
+    return dSdt, dIdt, dRdt, dMdt
 
 # Initial conditions vector
-y0 = S0, I0, R0
+y0 = S0, I0, R0, M0
 # Integrate the SIR equations over the time grid, t.
 ret = odeint(deriv, y0, t, args=(beta, gamma))
-S, I, R = ret.T
+S, I, R, M = ret.T
 
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
@@ -39,6 +46,7 @@ ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
 ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
 ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+ax.plot(t, M/1000, 'y', alpha=0.5, lw=2, label='Death')
 ax.plot(t, (S+I+R)/1000, '--', alpha=0.5, lw=2, label='Total Population')
 ax.set_xlabel('Time /days')
 ax.set_ylabel('Quantity (*1000)')
